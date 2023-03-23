d0bc3ec1d143ffdc06064eaba9a3f261725d378b
joao jacinto <joaonetotfm@gmail.com>
2023-03-22 21:01:39 -0300

    hasPrecedence

--- infix_to_posfix.c
+++ infix_to_posfix.c
@@ -2,21 +2,55 @@
 #include <ctype.h>
 #include <stdbool.h>
 
-int isOperand(char c) {
+bool isOperand(char c)
+{
     // isdigit verifica se o caracter é um dígito
     // isalpha verfica se o caracter é uma letra
 
-    if (isdigit(c) || isalpha(c)) {
+    if (isdigit(c) || isalpha(c))
+    {
         return true;
-    } else {
+    }
+    else
+    {
         return false;
+    }
+}
+
+int returnPrecedence(char operator)
+{
+    switch (operator)
+    {
+    case '+':
+    case '-':
+        return 1;
+        break;
+    case '*':
+    case '/':
+        return 2;
+        break;
+    case '^':
+        return 3;
+        break;
+    }
+}
+
+int hasPrecedence(char operator_1, char operator_2)
+{
+    int precedenceOperator_1 = returnPrecedence(operator_1);
+    int precedenceOperator_2 = returnPrecedence(operator_2);
+
+    if (precedenceOperator_1 >= precedenceOperator_2){
+        return 1;
+    }
+    else{
+        return 0;
     }
 }
 
 int main()
 {
-    char c;
-    scanf("%c", &c);
-    printf("%i", isOperand(c));
+    printf("%i", hasPrecedence('-', '*'));
+
     return 0;
 }
