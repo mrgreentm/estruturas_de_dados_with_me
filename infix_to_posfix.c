1c90735e63a61094ca7644e7bd3a38c19c2dc24e
joao jacinto <joaonetotfm@gmail.com>
2023-03-24 21:52:28 -0300

    fix: corrige implementacao de conversao

--- infix_to_posfix.c
+++ infix_to_posfix.c
@@ -1,105 +1,91 @@
-#include "./utils/pilha.h"
 #include <stdio.h>
 #include <ctype.h>
 #include <stdbool.h>
+#include "./utils/pilha.h"
 #include <string.h>
 
 #define MAXCOLS 80
 
-bool isOperand(char c)
-{
-    // isdigit verifica se o caracter é um dígito
-    // isalpha verfica se o caracter é uma letra
+bool isOperand(char c) {
+    return isalnum(c);
+}
 
-    if (isdigit(c) || isalpha(c))
-    {
-        return true;
+int getPrecedence(char operator) {
+    switch (operator) {
+        case '+':
+        case '-':
+            return 1;
+        case '*':
+        case '/':
+            return 2;
+        case '^':
+            return 3;
+        default:
+            return 0;
     }
-    else
-    {
+}
+
+bool hasPrecedence(char operator1, char operator2) {
+    int precedence1 = getPrecedence(operator1);
+    int precedence2 = getPrecedence(operator2);
+
+    if (precedence1 == 0 || precedence2 == 0) {
         return false;
     }
+
+    return precedence1 >= precedence2;
 }
 
-int returnPrecedence(char operator)
-{
-    switch (operator)
-    {
-    case '+':
-    case '-':
-        return 1;
-    case '*':
-    case '/':
-        return 2;
-    case '^':
-        return 3;
-    }
-}
-
-bool hasPrecedence(char operator_1, char operator_2)
-{
-    int precedenceOperator_1 = returnPrecedence(operator_1);
-    int precedenceOperator_2 = returnPrecedence(operator_2);
-
-    if (precedenceOperator_1 >= precedenceOperator_2)
-    {
-        return true;
-    }
-    else
-    {
-        return false;
-    }
-}
-
-void infixToPosfix(char infix[], char posfix[])
-{
-    int position, underflow; // unde é para verificarmos mais tarde se houver underflow na pilha
-    int outposition = 0;
-    char topSymbol = '+'; // caracter de maior precedencia
-    char symbol;
+void infixToPosfix(char infix[], char posfix[]) {
+    int position = 0, outPosition = 0;
+    char symbol, topSymbol;
+    bool underflow;
     Stack operatorStack;
-    Stack copyStack;
-    operatorStack.top = -1; // qualquer pilha deve sempre iniciar com topo com valor -1 para indicar que está vazia
-    copyStack.top = -1;     // qualquer pilha deve sempre iniciar com topo com valor -1 para indicar que está vazia
+    operatorStack.top = -1;
 
-    for (position = 0; (symbol = infix[position]) != '\0'; position++)
-    {
-        if (isOperand(symbol))
-            posfix[outposition++] = symbol;
-        else
-        {
-            popAndTest(&operatorStack, &symbol, &underflow);
-            while (!underflow && hasPrecedence(topSymbol, symbol))
-            {
-                posfix[outposition++] = topSymbol;
-                popAndTest(&copyStack, &topSymbol, &underflow);
-            }
-            if (!underflow && symbol != ')')
-                push(&copyStack, topSymbol);
-            if (underflow || (symbol != ')'))
-                push(&operatorStack, symbol);
-            else
+    while ((symbol = infix[position++]) != '\0') {
+        if (isOperand(symbol)) {
+            posfix[outPosition++] = symbol;
+        } else if (symbol == '(') {
+            push(&operatorStack, symbol);
+        } else if (symbol == ')') {
+            while (!empty(&operatorStack) && operatorStack.items[operatorStack.top] != '(') {
                 topSymbol = pop(&operatorStack);
+                posfix[outPosition++] = topSymbol;
+            }
+
+            if (!empty(&operatorStack) && operatorStack.items[operatorStack.top] == '(') {
+                pop(&operatorStack);
+            }
+        } else {
+            while (!empty(&operatorStack) && hasPrecedence(operatorStack.items[operatorStack.top], symbol)) {
+                topSymbol = pop(&operatorStack);
+                posfix[outPosition++] = topSymbol;
+            }
+
+            push(&operatorStack, symbol);
         }
     }
-    while (!empty(&operatorStack))
-        posfix[outposition++] = pop(&operatorStack);
-    posfix[outposition] = '\0';
+
+    while (!empty(&operatorStack)) {
+        topSymbol = pop(&operatorStack);
+        posfix[outPosition++] = topSymbol;
+    }
+
+    posfix[outPosition] = '\0';
 }
 
-int main()
-{
-    char infix_expression[MAXCOLS];
-    char posfix_expression[MAXCOLS];
-    int position = 0;
+int main() {
+    char infix[MAXCOLS];
+    char posfix[MAXCOLS];
 
-    // calculando o tamanho da expressão
-    while ((infix_expression[position++] = getchar()) != '\n');
-    infix_expression[--position] = '\0';
-    printf("%s%s", "infixa: ", infix_expression);
-    infixToPosfix(infix_expression, posfix_expression);
-    printf("\n%s%s", "posfixa: ", posfix_expression);
+    printf("Digite a expressao infix: ");
+    fgets(infix, MAXCOLS, stdin);
 
+    infixToPosfix(infix, posfix);
+
+    printf("Expressao posfixa: %s\n", posfix);
 
     return 0;
 }
+
